acb96addf8071a9d1341db2e74f11779aa36040b
-Jipes <55793183+Jipes@users.noreply.github.com>
2020-11-28 17:02:08 +0200

    Update message.js

--- Discord/events/message.js
+++ Discord/events/message.js
@@ -1,19 +1,33 @@
-// Module
-module.exports = (client, message, db) => {
-    
-    // Ignore Bots
-    if (message.author.bot) return;
-  
-    // Prefix
-    if (message.content.indexOf(client.config.prefix) !== 0) return;
+// Modules
+const config = require("./../config.json");
+const db = require('quick.db');
 
-    // Lets
-    let args = message.content.slice(client.config.prefix.length).trim().split(/ +/g);
-    let command = args.shift().toLowerCase();
-    let cmd = client.commands.get(command);
-    let messge = message; 
-   
-    // Run
-    if (!cmd) return;
-    cmd.run(client, messge, args, config, db);
-  };
+// Export
+module.exports = async (client, message) => {
+
+    // Set
+    client.config = config;
+
+    // Check Prefix And Bot
+    if (!message.content.startsWith(config.prefix) || message.author.bot) return;
+
+    //Splits args and command
+    const args = message.content.slice(config.prefix.length).split(' ');
+    const commandName = args.shift().toLowerCase();
+
+    //Stores command from the commands folder
+    const cmd = client.commands.get(commandName);
+
+    //If command doesnt exist then returns
+    if (!cmd) {
+    message.channel.send("That's not a command!")
+    return
+    };
+
+    try {
+        cmd.execute(message, args, client);
+    } catch (error) {
+        console.log(error);
+        message.reply('There was an error trying to execute that command!');
+    }
+}
