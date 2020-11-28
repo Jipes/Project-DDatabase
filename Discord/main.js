6dbef3c7a541acb45e87481ec55229ffceb0642d
-Jipes <55793183+Jipes@users.noreply.github.com>
2020-11-28 17:06:21 +0200

    Update main.js

--- Discord/main.js
+++ Discord/main.js
@@ -1,43 +1,38 @@
-// Consts
-const config = require("./config.json");
+// Modules
 const Discord = require("discord.js");
 const fs = require("fs");
 const client = new Discord.Client();
-const enmap = require("enmap")
-const db = new enmap({name: "db"})
+const config = require("./config.json");
+const path = require('path')
+const db = require('quick.db');
 
-// Config
+// Set 
+client.commands = new Discord.Collection();
+
 client.config = config;
 
-// Events
-fs.readdir("./events", (err, files) => {
-    if (err) return console.error(err);
-    files.forEach(file => {
-      const event = require(`./events/${file}`);
-      let eventName = file.split(".")[0];
-      client.on(eventName, event.bind(null, client));
-    });
-});
 
-// Commands
-client.commands = new Discord.Collection();
-fs.readdir("./commands/", (err, files) => {
-    if (err) return console.error(err);
-    files.forEach(file => {
-      if (!file.endsWith(".js")) return;
-      let props = require(`./commands/${file}`);
-      let commandName = file.split(".")[0];
-      console.log(`Loading command: ${commandName}`);
-      client.commands.set(commandName, props);
-    });
-});
+// Handler Finder
+fs.readdir('./handlers/', (err, files) => {
+    if (err) return console.error(err)
 
-// Shutdown
-process.on( "SIGINT", function() {
-	console.log("\nShutting down silently.")
-	client.destroy()
-	process.exit()
-})
+    files.forEach(file => {
+
+        const event = require(path.join(__dirname, "handlers", file));
+        event.run(client);
+
+    })
+
+    // Shut Down
+    process.on( "SIGINT", function() {
+    console.log("\nShutting down.")
+    client.destroy()
+    process.exit()
+});
 
 // Login
 client.login(config.token);
+
+// Export
+exports.client = client;
+})
